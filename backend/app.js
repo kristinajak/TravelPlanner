20170c1044096a678407cd44831c9e433bc8cefa
kristinajak <kristina.jakubone@gmail.com>
2023-09-04 16:59:33 +0300

    initial itinerary added

--- backend/app.js
+++ backend/app.js
@@ -270,6 +270,50 @@ app.post("/checklist/updateChecklist", async (req, res) => {
   }
 });
 
+app.get("/itinerary", async (req, res) => {
+  if (req.session.userId) {
+    const userId = req.session.userId;
+    const conn = await pool.getConnection();
+    const [rows, fields] = await conn.query(
+      `SELECT * FROM itinerary WHERE user_id = ${userId}`
+    );
+    conn.release();
+
+    const data = rows.map((row) => {
+      return {
+        id: row.id,
+        date: row.date,
+        city: row.city,
+        comments: row.comments,
+      };
+    });
+
+    res.json(data);
+  } else {
+    res
+      .status(500)
+      .json({ error: "An error occurred while getting itinerary data" });
+  }
+});
+
+app.post("/itinerary", async (req, res) => {
+  const userId = req.session.userId;
+  const newDate = req.body.date;
+  const newCity = req.body.city;
+  const newComments = req.body.comments;
+  console.log(userId, newCity, newDate, newComments);
+  const query = `INSERT INTO itinerary (date, city, comments, user_id) VALUES (?, ?, ?, ?)`;
+  const conn = await pool.getConnection();
+  const [result, fields] = await conn.execute(query, [
+    newDate,
+    newCity,
+    newComments,
+    userId,
+  ]);
+  conn.release();
+  res.sendStatus(200);
+});
+
 app.get("/logout", function (req, res) {
   console.log("Before destroying session");
   req.session.destroy(function (err) {
